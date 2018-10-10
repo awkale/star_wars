98b431dbfb2305e970668bfe9339629cf5b3767b
Alex Kale <hi@awkale.me>
2018-10-10 19:46:05 -0400

    change to use jumbtron

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,56 +1,15 @@
-import React, { Component } from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
-} from 'reactstrap';
+import React  from 'react';
+import { Jumbotron } from 'reactstrap';
 
-class Header extends Component {
-  state = {
-    isOpen: false,
-    characters: {}
-  };
-
-  toggle = () => {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  };
-
-  render() {
-    return (
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">Star Wars</NavbarBrand>
-        <NavbarToggler onClick={this.toggle} />
-        <Collapse isOpen={this.state.isOpen} navbar>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>Option 1</DropdownItem>
-                <DropdownItem>Option 2</DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>Reset</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    );
-  }
-}
+const Header = () => (
+  <div className="mt-4">
+    <Jumbotron className="bg-gradient-dark text-warning">
+      <h1 className="display-3">The Force Awakens data!</h1>
+      <p className="lead">
+        Choose a character below to learn more about them.
+      </p>
+    </Jumbotron>
+  </div>
+);
 
 export default Header;
