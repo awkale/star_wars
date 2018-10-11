25c6ef6eea836c2b946a098d98bf6ecfbac74b13
Alex Kale <hi@awkale.me>
2018-10-11 11:29:31 -0400

    make active link work

--- src/components/MainCharacters.js
+++ src/components/MainCharacters.js
@@ -3,25 +3,22 @@ import sampleCharacters from '../data/sampleCharacters.json';
 import { Nav, NavItem, NavLink } from 'reactstrap';
 
 class MainCharacters extends Component {
-  listCharacters = sampleCharacters.characters.map((char, i) => {
-    return (
-      <NavItem key={i}>
-        <NavLink
-          href="#"
-          active={char.name === this.props.charSelected}
-          onClick={() => this.props.getCharInfo(char.name, char.url)}
-        >
-          {char.name}
-        </NavLink>
-      </NavItem>
-    );
-  });
-
   render() {
     return (
       <div>
         <Nav pills vertical>
-          {this.listCharacters}
+          {sampleCharacters.characters.map((char, i) => (
+            <NavItem key={i}>
+              <NavLink
+                href="#"
+                active={char.name === this.props.charSelected}
+                onClick={() => this.props.getCharInfo(char.name, char.url)}
+              >
+                {char.name}
+              </NavLink>
+            </NavItem>
+            )
+          )}
         </Nav>
       </div>
     );
